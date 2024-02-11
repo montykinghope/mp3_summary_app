cb0f9ecc4624e5c2ec0c7c218dce93819e04bd79
Monty <montykinghope@gmail.com>
2024-02-10 20:30:17 -0500

    First commit

---
+++ whisper_function.py
@@ -0,0 +1,30 @@
+
+
+def audio_summarize(audio_file):
+
+    from openai import OpenAI
+    import openai
+
+    # Whisper API Key
+    client = OpenAI(api_key="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")
+
+    # Whisper Completion
+    audio_file = open(audio_file, "rb")
+    transcript = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text"
+    )
+
+    # GPT API Key
+    client = OpenAI(api_key="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")
+
+    # GPT Completion
+    chat_completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": transcript}],
+        model="gpt-3.5-turbo",
+        temperature=0,
+    )
+    reply_content = chat_completion.choices[0].message.content
+
+    return transcript, reply_content
